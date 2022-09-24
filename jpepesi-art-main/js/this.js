da33af8855de0ad3b3f79db2663e8462e59cb0c4
JPCodes44 <75552749+JPCodes44@users.noreply.github.com>
2022-09-24 17:01:01 -0400

    Add files via upload

---
+++ jpepesi-art-main/js/this.js
@@ -0,0 +1,8 @@
+var mobileLinks = document.getElementById("mobileLinks");
+
+function showMenu(){
+    mobileLinks.style.left= "0";
+}
+function hideMenu(){
+    mobileLinks.style.left = "-200%"; 
+}

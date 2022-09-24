0a4ff7882a31342b8fc3245a3c3bbf9865652eca
JPCodes44 <75552749+JPCodes44@users.noreply.github.com>
2022-09-24 17:01:46 -0400

    Delete jpepesi-art-main directory

--- jpepesi-art-main/README.md
+++
@@ -1,2 +0,0 @@
-# jpepesi-art
-cool shit for art
--- jpepesi-art-main/js/this.js
+++
@@ -1,8 +0,0 @@
-var mobileLinks = document.getElementById("mobileLinks");
-
-function showMenu(){
-    mobileLinks.style.left= "0";
-}
-function hideMenu(){
-    mobileLinks.style.left = "-200%"; 
-}
